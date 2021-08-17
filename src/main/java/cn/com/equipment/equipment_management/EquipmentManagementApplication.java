b58d10013f87ff8951cafd431ea6d561a8083d00
86186 <sxfhappy@163.com>
2021-08-17 09:49:19 +0800

    Initial commit

---
+++ src/main/java/cn/com/equipment/equipment_management/EquipmentManagementApplication.java
@@ -0,0 +1,13 @@
+package cn.com.equipment.equipment_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquipmentManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EquipmentManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/com/equipment/equipment_management/EquipmentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.equipment.equipment_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquipmentManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
